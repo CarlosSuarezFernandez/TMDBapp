dc7408e69741cbdba79ab7a10f5c70b2fd049168
CarlosSuarezFernandez <carlos.suarez.fernandez17@gmail.com>
2023-11-29 21:21:41 +0100

    app module

--- app/src/main/java/com/carlosdev/tmdbapp/di/appModule.kt
+++ app/src/main/java/com/carlosdev/tmdbapp/di/appModule.kt
@@ -1,7 +1,9 @@
 package com.carlosdev.tmdbapp.di
 
+import com.carlosdev.tmdbapp.data.MovieRepositoryImpl
+import com.carlosdev.tmdbapp.domain.repository.MovieRepository
 import org.koin.dsl.module
 
 val appModule = module{
-
+    single<MovieRepository> {MovieRepositoryImpl()}
 }
