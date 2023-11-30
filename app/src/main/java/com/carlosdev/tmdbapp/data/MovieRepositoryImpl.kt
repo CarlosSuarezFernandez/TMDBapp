5099dbf785b1c753725db05f655a2b6016f039ff
CarlosSuarezFernandez <carlos.suarez.fernandez17@gmail.com>
2023-11-30 13:00:49 +0100

    updated repository

--- app/src/main/java/com/carlosdev/tmdbapp/data/MovieRepositoryImpl.kt
+++ app/src/main/java/com/carlosdev/tmdbapp/data/MovieRepositoryImpl.kt
@@ -6,30 +6,59 @@ import app.moviebase.tmdb.model.TmdbMediaType
 import app.moviebase.tmdb.model.TmdbMovieDetail
 import app.moviebase.tmdb.model.TmdbMoviePageResult
 import app.moviebase.tmdb.model.TmdbStatusResult
+import com.carlosdev.tmdbapp.domain.model.OutcomeState
+import com.carlosdev.tmdbapp.domain.repository.FavoriteResponse
+import com.carlosdev.tmdbapp.domain.repository.MovieDetailResponse
 import com.carlosdev.tmdbapp.domain.repository.MovieRepository
+import com.carlosdev.tmdbapp.domain.repository.MoviesResponse
 
-class MovieRepositoryImpl(): MovieRepository {
+class MovieRepositoryImpl() : MovieRepository {
 
     override val tmdb = Tmdb3(API_KEY)
 
-    override suspend fun popularMovies(page: Int): TmdbMoviePageResult {
-        return tmdb.movies.popular(page, ENGLISH)
+    override suspend fun popularMovies(page: Int): MoviesResponse {
+        return try {
+            val movies = tmdb.movies.popular(page, ENGLISH)
+            OutcomeState.Success(movies)
+        } catch (e: Exception) {
+            OutcomeState.error(e)
+        }
     }
 
-    override suspend fun movieDetail(id: Int): TmdbMovieDetail {
-        return tmdb.movies.getDetails(id, ENGLISH, listOf(AppendResponse.MOVIE_CREDITS))
+    override suspend fun movieDetail(id: Int): MovieDetailResponse {
+        return try {
+            val movie = tmdb.movies.getDetails(id, ENGLISH, listOf(AppendResponse.MOVIE_CREDITS))
+            OutcomeState.Success(movie)
+        } catch (e: Exception) {
+            OutcomeState.error(e)
+        }
     }
 
-    override suspend fun favoriteMovies(): TmdbMoviePageResult {
-        return tmdb.account.getFavoriteMovies(ACCOUNT_ID)
+    override suspend fun favoriteMovies(): MoviesResponse {
+        return try {
+            val movies = tmdb.account.getFavoriteMovies(ACCOUNT_ID)
+            OutcomeState.Success(movies)
+        } catch (e: Exception) {
+            OutcomeState.error(e)
+        }
     }
 
-    override suspend fun addFavorite(id: Int): TmdbStatusResult {
-        return tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, true)
+    override suspend fun addFavorite(id: Int): FavoriteResponse {
+        return try {
+            val response = tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, true)
+            OutcomeState.Success(response)
+        } catch (e: Exception) {
+            OutcomeState.error(e)
+        }
     }
 
-    override suspend fun removeFavorite(id: Int): TmdbStatusResult {
-        return tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, false)
+    override suspend fun removeFavorite(id: Int): FavoriteResponse {
+        return try {
+            val response = tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, false)
+            OutcomeState.Success(response)
+        } catch (e: Exception) {
+            OutcomeState.error(e)
+        }
     }
 
     companion object {
--- app/src/main/java/com/carlosdev/tmdbapp/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/carlosdev/tmdbapp/domain/repository/MovieRepository.kt
@@ -4,12 +4,17 @@ import app.moviebase.tmdb.Tmdb3
 import app.moviebase.tmdb.model.TmdbMovieDetail
 import app.moviebase.tmdb.model.TmdbMoviePageResult
 import app.moviebase.tmdb.model.TmdbStatusResult
+import com.carlosdev.tmdbapp.domain.model.OutcomeState
+
+typealias MoviesResponse = OutcomeState<TmdbMoviePageResult>
+typealias MovieDetailResponse = OutcomeState<TmdbMovieDetail>
+typealias FavoriteResponse = OutcomeState<TmdbStatusResult>
 
 interface MovieRepository {
     val tmdb: Tmdb3
-    suspend fun popularMovies(page: Int): TmdbMoviePageResult
-    suspend fun movieDetail(id: Int): TmdbMovieDetail
-    suspend fun favoriteMovies(): TmdbMoviePageResult
-    suspend fun addFavorite(id: Int): TmdbStatusResult
-    suspend fun removeFavorite(id: Int): TmdbStatusResult
+    suspend fun popularMovies(page: Int): MoviesResponse
+    suspend fun movieDetail(id: Int): MovieDetailResponse
+    suspend fun favoriteMovies(): MoviesResponse
+    suspend fun addFavorite(id: Int): FavoriteResponse
+    suspend fun removeFavorite(id: Int): FavoriteResponse
 }
