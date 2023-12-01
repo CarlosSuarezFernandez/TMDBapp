5055c2464a6740db4b77cd43cbd5e5b5df5660e9
CarlosSuarezFernandez <carlos.suarez.fernandez17@gmail.com>
2023-12-01 22:27:36 +0100

    repository fix

--- app/src/main/java/com/carlosdev/tmdbapp/data/MovieRepositoryImpl.kt
+++ app/src/main/java/com/carlosdev/tmdbapp/data/MovieRepositoryImpl.kt
@@ -17,48 +17,24 @@ class MovieRepositoryImpl() : MovieRepository {
     override val tmdb = Tmdb3(API_KEY)
 
     override suspend fun popularMovies(page: Int): MoviesResponse {
-        return try {
-            val movies = tmdb.movies.popular(page, ENGLISH)
-            OutcomeState.Success(movies)
-        } catch (e: Exception) {
-            OutcomeState.error(e)
-        }
+        val response = tmdb.movies.popular(page, ENGLISH).results
+        return response
     }
 
     override suspend fun movieDetail(id: Int): MovieDetailResponse {
-        return try {
-            val movie = tmdb.movies.getDetails(id, ENGLISH, listOf(AppendResponse.MOVIE_CREDITS))
-            OutcomeState.Success(movie)
-        } catch (e: Exception) {
-            OutcomeState.error(e)
-        }
+        return tmdb.movies.getDetails(id, ENGLISH, listOf(AppendResponse.MOVIE_CREDITS))
     }
 
     override suspend fun favoriteMovies(): MoviesResponse {
-        return try {
-            val movies = tmdb.account.getFavoriteMovies(ACCOUNT_ID)
-            OutcomeState.Success(movies)
-        } catch (e: Exception) {
-            OutcomeState.error(e)
-        }
+        return tmdb.account.getFavoriteMovies(ACCOUNT_ID).results
     }
 
     override suspend fun addFavorite(id: Int): FavoriteResponse {
-        return try {
-            val response = tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, true)
-            OutcomeState.Success(response)
-        } catch (e: Exception) {
-            OutcomeState.error(e)
-        }
+        return tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, true)
     }
 
     override suspend fun removeFavorite(id: Int): FavoriteResponse {
-        return try {
-            val response = tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, false)
-            OutcomeState.Success(response)
-        } catch (e: Exception) {
-            OutcomeState.error(e)
-        }
+        return tmdb.account.markFavorite(ACCOUNT_ID, TmdbMediaType.MOVIE, id, false)
     }
 
     companion object {
--- app/src/main/java/com/carlosdev/tmdbapp/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/carlosdev/tmdbapp/domain/repository/MovieRepository.kt
@@ -1,14 +1,15 @@
 package com.carlosdev.tmdbapp.domain.repository
 
 import app.moviebase.tmdb.Tmdb3
+import app.moviebase.tmdb.model.TmdbMovie
 import app.moviebase.tmdb.model.TmdbMovieDetail
 import app.moviebase.tmdb.model.TmdbMoviePageResult
 import app.moviebase.tmdb.model.TmdbStatusResult
 import com.carlosdev.tmdbapp.domain.model.OutcomeState
 
-typealias MoviesResponse = OutcomeState<TmdbMoviePageResult>
-typealias MovieDetailResponse = OutcomeState<TmdbMovieDetail>
-typealias FavoriteResponse = OutcomeState<TmdbStatusResult>
+typealias MoviesResponse = List<TmdbMovie>
+typealias MovieDetailResponse = TmdbMovieDetail
+typealias FavoriteResponse = TmdbStatusResult
 
 interface MovieRepository {
     val tmdb: Tmdb3
