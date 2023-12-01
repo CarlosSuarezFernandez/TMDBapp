332036d03490661c06bddefcfdd221249257ede4
CarlosSuarezFernandez <carlos.suarez.fernandez17@gmail.com>
2023-12-01 22:25:28 +0100

    viewmodel module

--- app/src/main/java/com/carlosdev/tmdbapp/di/appModule.kt
+++ app/src/main/java/com/carlosdev/tmdbapp/di/appModule.kt
@@ -2,8 +2,11 @@ package com.carlosdev.tmdbapp.di
 
 import com.carlosdev.tmdbapp.data.MovieRepositoryImpl
 import com.carlosdev.tmdbapp.domain.repository.MovieRepository
+import com.carlosdev.tmdbapp.presentation.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module{
     single<MovieRepository> {MovieRepositoryImpl()}
+    viewModel {HomeViewModel(get())}
 }
